f7105810416662d8af2bdbb83c0dd822c4b649dd
Italo Morales F <i@italomoralesf.com>
2018-05-28 16:10:01 -0500

    route api and controller

--- app/Http/Controllers/api/LessonController.php
+++ app/Http/Controllers/api/LessonController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\api;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+use App\Lesson;
 
 class LessonController extends Controller
 {
-    //
+    
+    public function getLessons()
+    {
+    	$lessons = Lesson::get();
+
+   		return $lessons;
+    }
+
+    public function getLesson($slug)
+    {
+    	$lesson = Lesson::where('slug', $slug)->first();
+
+    	return $lesson;
+    }
+
 }
